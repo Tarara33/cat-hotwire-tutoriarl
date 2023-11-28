9bb4b85f53d260f967784d35b922e2ba2ab284f1
Sarina <sarina0618@gmail.com>
2023-11-28 19:07:31 +0900

    ✨ turbo stream化

--- app/controllers/cats_controller.rb
+++ app/controllers/cats_controller.rb
@@ -26,7 +26,7 @@ class CatsController < ApplicationController
     @cat = Cat.new(cat_params)
 
     if @cat.save
-      redirect_to @cat, notice: "ねこを登録しました。"
+      flash.now.notice = "ねこを登録しました。"
     else
       render :new, status: :unprocessable_entity
     end
@@ -35,7 +35,8 @@ class CatsController < ApplicationController
   # PATCH/PUT /cats/1
   def update
     if @cat.update(cat_params)
-      redirect_to @cat, notice: "ねこを更新しました。"
+      # リダイレクトを削除（リダイレクトがないと暗黙的に`render`が実行される）
+      flash.now.notice = "ねこを更新しました。"
     else
       render :edit, status: :unprocessable_entity
     end
@@ -44,7 +45,7 @@ class CatsController < ApplicationController
   # DELETE /cats/1
   def destroy
     @cat.destroy
-    redirect_to cats_url, notice: "ねこを削除しました。"
+    flash.now.notice = "ねこを削除しました。"
   end
 
   private
